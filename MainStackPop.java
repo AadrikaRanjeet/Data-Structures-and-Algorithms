49e7d7c3dbeba71a5539fe95d97fe852c6c4fd52
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-06-14 07:53:17 +0530

    Add files via upload
    The following code is about Pop operation in Stack

---
+++ MainStackPop.java
@@ -0,0 +1,9 @@
+public class MainStackPop {
+    public static void main(String [] args)
+    {
+        StackPop newStack=new StackPop(7);
+        newStack.Pop();
+        newStack.PrintStack();
+    }
+}
+
