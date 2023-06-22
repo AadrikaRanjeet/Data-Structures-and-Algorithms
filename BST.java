55f3991d9138cefdfe013c2a0a7d3b8c50cb978c
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-06-22 15:02:42 +0530

    Add files via upload

---
+++ BST.java
@@ -0,0 +1,16 @@
+public class BST {
+    Node root;
+    //constructor banane ki zaroorat nhi kyunki root already = null hota hai 
+    class Node 
+    {
+        int value;
+        Node left;
+        Node right;
+
+        private Node(int value)
+        {
+            this.value=value;
+        }
+    }
+    
+}
