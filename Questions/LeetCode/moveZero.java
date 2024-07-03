05913ac70e7dfcb49d168d0a8456cb41fc0c3eed
Aadrika Ranjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2024-07-03 11:24:29 +0530

    Add files via upload

---
+++ Questions/LeetCode/moveZero.java
@@ -0,0 +1,47 @@
+/*iven an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0] */
+
+
+public class moveZero
+{
+    public int[] moveZeroes(int[] arr) {
+        int j=-1;
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                j=i;
+                break;
+            }
+        }
+        if(j==-1)
+        {
+            return arr;
+        }
+        //swap 
+        for(int i=j+1;i<n;i++)
+        {
+            if(arr[i]!=0)
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        return arr;
+    }
+}
